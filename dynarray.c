f08a9d10db013878dad3b6c6a48324fdfa992c29
Adrien Baur <abaur@student.42.fr>
2020-02-11 14:40:00 +0100

    Added dynarray prototypes.

---
+++ dynarray.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dynarray.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/11 14:29:36 by abaur             #+#    #+#             */
+/*   Updated: 2020/02/11 14:39:17 by abaur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "dynarray.h"
+
+/*
+** Initializes a new dynarray.
+** @param t_dynarray* this The array to initialize.
+** @param size_t type	The size of a single element of the stored type.
+** @param size_t capacity	The initial size of the array.
+** @return bool
+** 	true  OK
+**	false Allocation failed.
+*/
+
+extern short	dynarrayinit(t_dynarray *this, size_t type, size_t capacity)
+{
+}
+
+/*
+** Makes sure the array can contain more elements.
+** @param t_dynarray* this	The array to expand.
+** @param unsigned int amount	The amount of extra elements needed.
+** @return bool
+** 	true  OK
+** 	false allocation failed.
+*/
+
+extern short	arrayexpand(t_dynarray *this, unsigned int amount)
+{
+}
+
+#endif
--- dynarray.h
+++ dynarray.h
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 14:16:38 by abaur             #+#    #+#             */
-/*   Updated: 2020/02/11 14:27:34 by abaur            ###   ########.fr       */
+/*   Updated: 2020/02/11 14:32:34 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 /*
 ** @var size_t size	The total size of the array object.
+** @var size_t type	The size of each element.
 ** @var size_t capacity	The maximum capacity of the array object.
 ** @var size_t length	The current amount of items in the array.
 ** @var void* content	The actual content of the array.
@@ -26,12 +27,13 @@ typedef struct s_dynarray	t_dynarray;
 struct	s_dynarray
 {
 	size_t	size;
+	size_t	type;
 	size_t	capacity;
 	size_t	length;
 	void	*content;
 }
 
-short	dynarrayinit(t_dynarray *this);
+short	dynarrayinit(t_dynarray *this, size_t type, size_t capacity);
 short	arrayexpand(t_dynarray *this,  unsigned int amount);
 
 #endif
